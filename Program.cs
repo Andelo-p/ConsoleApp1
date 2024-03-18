7d8eabfe06ac1b49a99c40d30ee85bf1839b6fa6
kbarbaric <115696047+kbarbaric@users.noreply.github.com>
2024-03-18 17:45:50 +0100

    Updated Program.cs
    added Hello, World 2! and comment at the end of file

--- Program.cs
+++ Program.cs
@@ -5,4 +5,6 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World 2!");
 
+//added line 8  --> Console.WriteLine("Hello, World 2!");
